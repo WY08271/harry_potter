a9e192ccdc922bda75899a95f6a04665e0362d94
wy <wy08271@outlook.com>
2015-09-17 10:42:52 +0800

    add soec/cart-spec.js and src/cart.js

---
+++ spec/cart-spec.js
@@ -0,0 +1,31 @@
+'use _strict';
+
+var Cart = require('../src/cart.js');
+
+describe('Cart', function() {
+  var cart;
+  beforeEach(function() {
+    cart = new Cart();
+  });
+
+  describe('#getCount()', function() {
+    it('can be get number of book', function() {
+      var input =[{name : 'harry-potter-one', price : 8.00, count : 1},
+                 {name : 'harry-potter-one', price : 8.00, count : 1},
+                 {name : 'harry-potter-one', price : 8.00, count : 1}];
+      result = cart.getCount(input);
+
+      expect(result[0].count).toBe(3);
+    });
+  });
+
+  describe('#findBook()', function() {
+    it('can find book exist booklist', function() {
+      cart.booklist = [{name : 'harry-potter-one', price : 8.00, count : 1},
+                      {name : 'harry-potter-two', price : 8.00, count : 2}];
+      result = cart.findBook('harry-potter-two');
+
+      expect(result).toEqual({name : 'harry-potter-two', price : 8.00, count : 2});
+    });
+  });
+});
---
+++ src/cart.js
@@ -0,0 +1,30 @@
+var Book = require('./book');
+
+function Cart ( book, count, booklist) {
+  this.book = book || new Book();
+  this.count = count || 1;
+  this.booklist = [];
+}
+
+Cart.prototype.getCount = function (input){
+  for(var i = 0;i < input.length; i++){
+    var index = this.findBook(input[i].name);
+
+    if(index) {
+      index.count += this.count;
+    }else {
+      this.booklist.push({name : input[i].name, price : input[i].price, count : this.count});
+    }
+  }
+  return this.booklist;
+};
+
+Cart.prototype.findBook = function (name){
+  for(var i = 0; i < this.booklist.length; i++){
+    if(this.booklist[i].name === name){
+      return this.booklist[i];
+    }
+  }
+};
+
+module.exports = Cart;
