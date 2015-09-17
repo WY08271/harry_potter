173491f3c5c7f1f929d7fd28598068783a51effd
wy <wy08271@outlook.com>
2015-09-17 20:55:00 +0800

    add printReceipt to main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 var Cart = require('./cart');
 var Money = require('./money');
 var DiscountPlan = require('./discountPlan');
+var Receipt = require('./receipt');
 
 function harryPotter(input){
   var cart = new Cart();
   var booklist = cart.getCount(input);
 
   var discountPlan = new DiscountPlan();
-  discountPlan.group(booklist);
+  var plan = discountPlan.group(booklist);
 
   var discountPrice = discountPlan.calculate();
   var money = new Money();
   var actualMoney = money.actualMoney(booklist, discountPrice);
+  var receipt = new Receipt();
+  var string = receipt.printReceipt(booklist, plan);
 
   return actualMoney;
 }
