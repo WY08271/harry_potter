014f27a439d4923f6c03b3882f52d3cf106fe933
wy <wy08271@outlook.com>
2015-09-17 18:14:29 +0800

    add money.js and money-spec.js

---
+++ spec/money-spec.js
@@ -0,0 +1,31 @@
+'use _strict';
+
+var Money = require('../src/money.js');
+
+describe('Money', function() {
+  var money;
+  beforeEach(function() {
+    money = new Money();
+  });
+
+  describe('#totalMoney()', function() {
+    it('can calculate no discount price', function() {
+      var input =[{name : 'harry-potter-one', price : 8.00, count : 1},
+                 {name : 'harry-potter-one', price : 8.00, count : 1},
+                 {name : 'harry-potter-one', price : 8.00, count : 1}];
+
+      expect(money.totalMoney(input)).toBe(24);
+    });
+  });
+
+  describe('#actualMoney()', function() {
+    it('can calculate have discount price', function() {
+      var input =[{name : 'harry-potter-one', price : 8.00, count : 1},
+                 {name : 'harry-potter-one', price : 8.00, count : 1},
+                 {name : 'harry-potter-one', price : 8.00, count : 1}];
+      var totalMoney = money.totalMoney(input);
+
+      expect(money.actualMoney(totalMoney,10)).toBe(14);
+    });
+  });
+});
---
+++ src/money.js
@@ -0,0 +1,19 @@
+function Money () {
+
+}
+
+Money.prototype.totalMoney = function(booklist){
+  var totalMoney = 0;
+
+  for(var i = 0; i < booklist.length; i++){
+    totalMoney += booklist[i].price * booklist[i].count;
+  }
+
+  return totalMoney;
+};
+
+Money.prototype.actualMoney = function(totalMoney, discountPrice){
+  return totalMoney - discountPrice;
+};
+
+module.exports = Money;
