ddb8901eaff0878f562234e2215d5aa4a4f898b8
wy <wy08271@outlook.com>
2015-09-20 22:22:40 +0800

    increased test coverage

--- spec/discountPlan-spec.js
+++ spec/discountPlan-spec.js
@@ -22,12 +22,45 @@ describe('DiscountPlan', function() {
   });
 
   describe('#calculate()', function() {
-    it('can calculate the discount of books', function() {
+    it('can calculate the discount of two same books and one book', function() {
       var booklist = [{name : 'harry-potter-one', price : 8.00, count : 1},
                       {name : 'harry-potter-two', price : 8.00, count : 2}];
       discountPlan.group(booklist);
-      
+
       expect(discountPlan.calculate()).toBe(0.8);
+    });
+
+    it('can calculate the discount of one book', function() {
+      var booklist = [{name : 'harry-potter-one', price : 8.00, count : 1}];
+      discountPlan.group(booklist);
+
+      expect(discountPlan.calculate()).toBe(0);
+    });
+
+    it('can calculate the discount of two same books', function() {
+      var booklist = [{name : 'harry-potter-one', price : 8.00, count : 2}];
+      discountPlan.group(booklist);
+
+      expect(discountPlan.calculate()).toBe(0);
+    });
+
+    it('can calculate the discount of two different books', function() {
+      var booklist = [{name : 'harry-potter-one', price : 8.00, count : 1},
+                      {name : 'harry-potter-two', price : 8.00, count : 1}];
+      discountPlan.group(booklist);
+
+      expect(discountPlan.calculate()).toBe(0.8);
+    });
+
+    it('can calculate the discount of eight different books', function() {
+      var booklist = [{name : 'harry-potter-one', price : 8.00, count : 2},
+                      {name : 'harry-potter-two', price : 8.00, count : 2},
+                      {name : 'harry-potter-three', price : 8.00, count : 2},
+                      {name : 'harry-potter-four', price : 8.00, count : 1},
+                      {name : 'harry-potter-five', price : 8.00, count : 1}];
+      discountPlan.group(booklist);
+
+      expect(discountPlan.calculate()).toBe(12.4);
     });
   });
 });
